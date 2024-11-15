5ebc11b1dd1b03263e6337e8abc304f8d8685935
sudharsan <sudharsansuppaiah@gmail.com>
2024-11-15 07:35:08 +0530

    next project

---
+++ new.cpp
@@ -0,0 +1,17 @@
+#include<iostream>
+#include<cstring>
+using namespace std;
+int main(){
+    int i,j,length;
+    string str1;
+    cout<<"enter ther string";
+    cin>>str1;
+     j=0;
+     length=strlen(str1);
+    for(int i=length-1;i>=0;i--){
+         [i]
+;
+
+     }
+    }
+}
